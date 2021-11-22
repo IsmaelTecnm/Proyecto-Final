f9bdc1d66054598a46e35a47a5f3715a26e3e31b
IsmaelTecnm <ismael_suarez18@outlook.es>
2021-11-22 15:15:37 -0700

    Se creo el archivo conexión

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php 
+	$host="localhost";
+	$usuario="id16967209_proyectobduser";
+	$contraseña="$]q{n(A@pi2xgVG|";
+	$base="id16967209_proyectobd";
+	$conexion=new mysqli($host,$usuario,$contraseña,$base);
+	if($conexion -> connect_errno)
+	{
+	die("Fallo la conexion:(".$conexion->mysqli_connect_errno().")".$conexion->mysqli_connect_errno());
+	}
+?>
